fc055093a8b689fd029f9bce5d61078ffa608e2c
Angel G <agarcia@pasiona.com>
2017-11-14 17:45:11 +0100

    Adds the Azure function to perform restarts.
    This Azure Function of type Generic Webhook receives a request and uses
    its data to determine which WebApp invokes the function and restart it
    if the status of the request received is different from "Resolved".

---
+++ run.ps1
@@ -0,0 +1,26 @@
+# Get the input request
+$in = Get-Content $req -Raw
+$json = ConvertFrom-Json $in
+
+if ($json.status -eq "Resolved") {
+   Write-output "Status is " $json.status
+}
+else {
+    Write-Output $json.status
+    Write-Output $json.context.resourceGroupName
+    Write-Output $json.context.resourceName
+    
+    # Application ID for our Azure Security Principal that we created and provided via Function Application Settings
+    $username = $env:AzureAutomationAppID
+
+    # Password for connection to Azure via Function Application Settings
+    $pw = $env:AzureAutomationPWD
+    $key = Get-Content 'D:\home\site\wwwroot\<your_function>\<yourkeyfilename.key>'
+    $password = $pw | ConvertTo-SecureString -key $key
+    $credentials = New-Object System.Management.Automation.PSCredential $username,$password
+    $AzureRMAccount = Add-AzureRmAccount -Credential $credentials -ServicePrincipal -TenantId $env:AzureAutomationTenantID
+    If ($AzureRMAccount) { 
+        Restart-AzureRmWebApp -ResourceGroupName $json.context.resourceGroupName -Name $json.context.resourceName
+        write-output "==== WebApp restarted ====" $json.context.resourceName
+    }
+}
