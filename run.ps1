3b50064366d36e5091a1c4235da85f4dd7420887
Angel <angel.g-santos@outlook.com>
2018-03-20 14:04:01 +0100

    Update function to print the received json object

--- run.ps1
+++ run.ps1
@@ -2,6 +2,8 @@
 $in = Get-Content $req -Raw
 $json = ConvertFrom-Json $in
 
+write-output $in
+
 if ($json.status -eq "Resolved") {
    Write-output "Status is " $json.status
 }
@@ -15,7 +17,7 @@ else {
 
     # Password for connection to Azure via Function Application Settings
     $pw = $env:AzureAutomationPWD
-    $key = Get-Content 'D:\home\site\wwwroot\<your_function>\<yourkeyfilename.key>'
+    $key = Get-Content 'D:\home\site\wwwroot\reboot\4sessions.key'
     $password = $pw | ConvertTo-SecureString -key $key
     $credentials = New-Object System.Management.Automation.PSCredential $username,$password
     $AzureRMAccount = Add-AzureRmAccount -Credential $credentials -ServicePrincipal -TenantId $env:AzureAutomationTenantID
